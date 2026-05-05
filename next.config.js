e38a73a2d144ca5642fcbbde6bae0d02b54aa800
mdfirdausr-lhdnm <m.firdausrom@gmail.com>
2026-05-05 14:16:50 +0800

    Make basePath conditional - only apply in production

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production'
+
 const nextConfig = {
   output: 'export',
-  basePath: '/masjid-ar-rahmah',
+  basePath: isProd ? '/masjid-ar-rahmah' : '',
   images: {
     unoptimized: true,
   },
